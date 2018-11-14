4d3168be03142ce019cffe934d96d87729dc5bbf
phuecksteadt <phuecksteadt@HBX1601D104361>
2018-11-13 16:17:23 -0800

    create CelestialObjectController class

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AppNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
